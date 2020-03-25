f57d393bdd142d2bcb38ea2252c6767227e75288
pavel.chausov <pavel.chausov@maxidom.ru>
2020-03-25 14:26:50 +0300

    шаг 7 lint fix

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -2,6 +2,7 @@
 
 import { greetings, game } from '../src/index.js';
 import getGCDRules from '../games/get-gcd-rules.js';
+
 const userName = greetings();
 const gameRules = getGCDRules();
 game({
--- games/get-gcd-rules.js
+++ games/get-gcd-rules.js
@@ -3,14 +3,14 @@ const gcd = (a, b) => {
     return a;
   }
   return gcd(b, a % b);
-}
+};
 
 const getGCDRules = () => {
   const rules = 'Find the greatest common divisor of given numbers.';
   const makeMove = () => {
     const number1 = Math.floor(Math.random() * Math.floor(100));
     const number2 = Math.floor(Math.random() * Math.floor(100));
-    const correctAnswer = gcd(number1, number2)
+    const correctAnswer = gcd(number1, number2);
     const question = `${number1} ${number2}`;
     return { question, correctAnswer };
   };
